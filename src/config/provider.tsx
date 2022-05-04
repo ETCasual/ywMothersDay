c0d2a62173cb145a7c7e18709ed9fa9d028c6a72
ETCasual <casualorient@gmail.com>
2022-05-04 19:00:46 +0800

    Fix React.FC

--- src/config/provider.tsx
+++ src/config/provider.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { FirebaseAppProvider } from 'reactfire';
 import { firebaseConfig } from './firebaseConfig';
 
-export const Provider: React.FC = ({ children }) => {
+interface ProviderProps {
+  children: React.ReactNode;
+}
+
+export const Provider: React.FC<ProviderProps> = ({ children }) => {
   return <FirebaseAppProvider firebaseConfig={firebaseConfig}>{children}</FirebaseAppProvider>;
 };
