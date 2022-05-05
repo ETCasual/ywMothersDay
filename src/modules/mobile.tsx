ffb04da51f38088e7d5eb9cdaaba2821bce0f074
ETCasual <casualorient@gmail.com>
2022-05-06 02:24:50 +0800

    Allowed multiple Submits

--- src/modules/mobile.tsx
+++ src/modules/mobile.tsx
@@ -7,7 +7,7 @@ import { ToastContainer, toast } from 'react-toastify';
 export const Mobile: React.FC = () => {
   const dbRef = useDatabase();
 
-  const { setSubmitted, submitted, value } = useSubmitted();
+  const { setSubmitted, value } = useSubmitted();
 
   const updateText = () => {
     return set(ref(dbRef, `/${encodeURI(name)}-${encodeURI(cg)}`), {
@@ -21,6 +21,7 @@ export const Mobile: React.FC = () => {
   const [name, setName] = useState<string>('');
   const [cg, setCG] = useState<string>('');
   const [text, setText] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(false);
 
   return typeof window !== 'undefined' ? (
     <>
@@ -29,14 +30,14 @@ export const Mobile: React.FC = () => {
           <p className="text-xl font-roboto font-bold text-primary mb-1">你的名字</p>
           <input
             className="rounded-md px-2 py-1 border-2 border-primary font-roboto mb-2"
-            disabled={submitted}
+            disabled={loading}
             onChange={(e) => (e.target.value ? setName(e.target.value) : null)}
             placeholder={value ? value.name : null}
           />
           <p className="text-xl font-roboto font-bold text-primary mb-1">你的小组</p>
           <input
             className="rounded-md px-2 py-1 border-2 border-primary font-roboto mb-2"
-            disabled={submitted}
+            disabled={loading}
             placeholder={value ? value.cg : null}
             onChange={(e) => (e.target.value ? setCG(e.target.value) : null)}
           />
@@ -45,27 +46,30 @@ export const Mobile: React.FC = () => {
             className="rounded-md border-2 px-2 py-1 border-primary font-roboto"
             rows={3}
             style={{ resize: 'none' }}
-            disabled={submitted}
+            disabled={loading}
             placeholder={value ? value.text : null}
             onChange={(e) => (e.target.value ? setText(e.target.value) : null)}
           />
           <button
             className="w-full px-2 py-1 font-roboto font-bold text-xl rounded-lg bg-primary text-white mt-3"
-            disabled={submitted}
+            disabled={loading}
             onClick={async () => {
               if (name && cg && text) {
-                await updateText().then(() => {
-                  setSubmitted({
-                    text: text,
-                    name: name,
-                    cg: cg,
-                  });
-                  toast('Submitted!', {
-                    type: 'success',
-                    position: 'bottom-center',
-                    theme: 'colored',
-                  });
-                });
+                setLoading(true);
+                await updateText()
+                  .then(() => {
+                    setSubmitted({
+                      text: text,
+                      name: name,
+                      cg: cg,
+                    });
+                    toast('Submitted!', {
+                      type: 'success',
+                      position: 'bottom-center',
+                      theme: 'colored',
+                    });
+                  })
+                  .finally(() => setLoading(false));
               }
             }}
           >
